670f60f2e8ffba9c7bb1bab428921fe71fdf17ae
Hinekureta <herrbach.kevin@gmail.com>
2017-12-13 23:45:03 +0100

    Renames Tests/ to tests/

--- tests/test.py
+++ tests/test.py
@@ -1,25 +1,16 @@
 import inspect
 
 class Test:
-
-    def __init__(self):
-        self.hasFailed = False
-
     def assertEqual(self, expected, actual):
-        return self.assertCondition(expected == actual)
+        self.assertCondition(expected == actual)
 
     def assertNone(self, element):
-        return self.assertCondition(element is None)
+        self.assertCondition(element is None)
 
     def assertNotIn(self, element, array):
-        return self.assertCondition(element not in array)
+        self.assertCondition(element not in array)
 
     def assertCondition(self, condition):
         message = inspect.stack()[2][3] + ' --- '
-        responseMessage = 'OK'
-        if not (condition):
-            responseMessage = 'FAILED'
-            self.hasFailed = True
-
-        message += responseMessage
+        message += 'OK' if condition else 'FAILED'
         print(message)
